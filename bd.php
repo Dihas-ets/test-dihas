b54104183497340ebb9e8ebb26b9fa0fd98fd783
Dihas-ets <diarrahouda@gmail.com>
2026-06-30 18:05:23 +0100

    file change

---
+++ bd.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = "localhost";
+$dbname = "based";
+$user = "root";
+$password = "";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+
+    die("Erreur de connexion : " . $e->getMessage());
+
+}
+?>
